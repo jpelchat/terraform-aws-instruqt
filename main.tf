0be382d7e060dc8f15421eb64a78dd809b3e95f2
jpelchat <83721525+jpelchat@users.noreply.github.com>
2023-12-14 10:30:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_instance" {
-  ami           = "var.aws_instance_ami"
-  instance_type = "var.aws_instance_size"
-  key_name      = "var.aws_instance_key"
+  ami           = var.aws_instance_ami
+  instance_type = var.aws_instance_size
+  key_name      = var.aws_instance_key
 
   tags = {
     Name = var.instance_name
