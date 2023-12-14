c1b8bad8bdd5ab729b8ba404f32f73d0e925c743
jpelchat <83721525+jpelchat@users.noreply.github.com>
2023-12-14 09:44:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = "var.aws_instance_region" 
+}
 
 resource "aws_instance" "ec2_instance" {
   ami           = "var.aws_instance_ami"
